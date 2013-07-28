197052c32691f10da9c8729aef8e4bd9dadb0e26
Mahbub Kabir <mahbub.kabir@gmail.com>
2013-07-28 13:53:49 +0100

    Class practices

---
+++ Week 11/svm.py
@@ -0,0 +1,11 @@
+﻿import pandas as pd
+data = pd.read_csv("/home/mahbub/Desktop/sourcecode/GA - Data Science/Week 11/train.csv")
+from sklearn.cross_validation import train_test_split
+df = data[['Survived','Pclass', 'Age']]
+df = df.dropna(subset=['Survived','Pclass', 'Age'], how='any')
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(df, df['Survived'], test_size=0.3, random_state=0)
+from sklearn import svm
+clf = svm.SVC()
+clf.fit(X_train, y_train) 
+testData = pd.read_csv("/home/mahbub/Desktop/sourcecode/GA - Data Science/Week 11/test.csv")
---
+++ Week 15/Exercise/totalwordcount.py
@@ -0,0 +1,37 @@
+﻿#!/usr/local/bin/python
+from mrjob.job import MRJob
+import itertools
+
+# ex1 - word count
+class MRWC(MRJob):
+
+    def mapper(self, _, line):
+
+        # print mapper input
+        # print line
+
+        for word in line.split():
+            # print mapper output
+            # print word, 1
+	# instead of using the word we are using a static keyword
+	# mapper is quivalent to GROUP BY of SQL, before we were doing something	# like SELECT count(word) GROUP BY word
+	# now we got rid of the GROUP BY and doing something like 
+	# SELECT COUNT(word)
+	
+            yield "totalCt", 1
+
+    def reducer(self, word, counts):
+
+        # print reducer input
+        # note - counts is a generator object, so we need to do some fancy
+        #        stuff to copy it (otherwise it's only good for a single use)
+        # tmp1, tmp2 = itertools.tee(counts)
+        # print word, [k for k in tmp1]
+        # yield word, sum(tmp2)
+
+        yield "totalCt", sum(counts)
+
+if __name__ == '__main__':
+    MRWC.run()
+    # MRInvIdx.run()
+    # MRWC2.run()
